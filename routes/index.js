42c55a8a66f373f2b4ce78d878b6fec2b9b4ac5c
Cecilia Ulrich <cecilia.ulrich@logicline.de>
2018-08-06 12:35:14 +0200

    Add new custom fields to the platform event

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,8 @@ router.get('/', function (req, res, next) {
     });
   } else {
     res.render('index', {
-      User: org.username, Instance: org.oauth.instance_url
+      User: org.username,
+      Instance: org.oauth.instance_url
     });
   }
 });
@@ -27,7 +28,8 @@ router.get('/token', function (req, res, next) {
     });
   } else {
     res.render('index', {
-      User: org.username, Instance: org.oauth.instance_url
+      User: org.username,
+      Instance: org.oauth.instance_url
     });
   }
 });
@@ -38,12 +40,15 @@ router.post('/configure', function (req, res, next) {
   module.exports = req.body;
   require('../lib/connection');
   res.render('index', {
-    User: org.username, Instance: org.oauth.instance_url
+    User: org.username,
+    Instance: org.oauth.instance_url
   });
 });
 
 router.get('/create-case', function (req, res, next) {
-  res.render('create-case', { title: 'Create Case' });
+  res.render('create-case', {
+    title: 'Create Case'
+  });
 });
 
 router.post('/publishEvent', function (req, res, next) {
@@ -51,7 +56,13 @@ router.post('/publishEvent', function (req, res, next) {
   event.set('CaseOrigin__c', req.body.origin);
   event.set('Serial_number__c', req.body.serialNumber);
   event.set('External_ID__c', req.body.externalId);
-  org.insert({ sobject: event }, err => {
+  event.set('Subject__c', req.body.subject);
+  event.set('Priority__c', req.body.priority);
+  event.set('Description__c', req.body.description);
+  event.set('Command__c', 'CREATE_CASE');
+  org.insert({
+    sobject: event
+  }, err => {
     if (err) {
       console.error(err);
     } else {
@@ -59,41 +70,59 @@ router.post('/publishEvent', function (req, res, next) {
     }
   });
   res.redirect('/cases');
+  socket.emit('event', { message: req.body.subject })
+
 });
 
 
 router.get('/create-account', function (req, res, next) {
-  res.render('create-account', { title: 'Create Account' });
+  res.render('create-account', {
+    title: 'Create Account'
+  });
 });
 
 router.get('/create-contact', function (req, res, next) {
-  res.render('create-contact', { title: 'Create Contact' });
+  res.render('create-contact', {
+    title: 'Create Contact'
+  });
 });
 
 router.get('/cases', function (req, res, next) {
 
-  org.query({ query: "Select Id, Serial_number__c ,External_ID__c, Subject, Origin From Case Order By LastModifiedDate DESC" })
+  org.query({
+      query: "Select Id, Serial_number__c ,External_ID__c, Subject, Origin, Priority, Status From Case Order By LastModifiedDate DESC"
+    })
     .then(function (results) {
-      res.render('index-cases', { records: results.records });
+      res.render('index-cases', {
+        records: results.records
+      });
     });
 
 });
 
 router.get('/accounts', function (req, res, next) {
 
-  org.query({ query: "Select Id, Name, Type, Industry, Rating From Account Order By LastModifiedDate DESC" })
+  org.query({
+      query: "Select Id, Name, Type, Industry, Rating From Account Order By LastModifiedDate DESC"
+    })
     .then(function (results) {
-      res.render('index-accounts', { records: results.records });
+      res.render('index-accounts', {
+        records: results.records
+      });
     });
 
 });
 
 router.get('/contacts', function (req, res, next) {
 
-  org.query({ query: "Select Id, Name, Email, Phone From Contact Order By Name" })
+  org.query({
+      query: "Select Id, Name, Email, Phone From Contact Order By Name"
+    })
     .then(function (results) {
       console.log('contacts: %s', String(results.records));
-      res.render('index-contacts', { records: results.records });
+      res.render('index-contacts', {
+        records: results.records
+      });
     });
 
 });
@@ -105,9 +134,13 @@ router.post('/create-account', function (req, res, next) {
   acc.set('Type', req.body.type);
 
 
-  org.insert({ sobject: acc })
+  org.insert({
+      sobject: acc
+    })
     .then(function (account) {
-      res.render('message', { title: 'Account Created: ' + String(account.id) });
+      res.render('message', {
+        title: 'Account Created: ' + String(account.id)
+      });
     })
 });
 
@@ -120,21 +153,45 @@ router.post('/create-contact', function (req, res, next) {
 
 
 
-  org.insert({ sobject: cont })
+  org.insert({
+      sobject: cont
+    })
     .then(function (contact) {
-      res.render('message', { title: 'Contact Created: ' + String(contact.id) });
+      res.render('message', {
+        title: 'Contact Created: ' + String(contact.id)
+      });
     })
 });
 
-router.post('/delete-case', function (req, res, next) {
+router.post('/manage-case', function (req, res, next) {
 
-  var cs = nforce.createSObject('Case');
-  cs.set('Id', req.body.id);
-  console.log('id: %s', req.body.id);
-  org.delete({ sobject: cs })
-    .then(function (msg) {
-      res.render('message', { title: 'Case Deleted: ' + String(req.body.id) });
+  if (req.body.close != null) {
+    var event = nforce.createSObject('IANA_case_creator__e');
+    event.set('CaseID__c', req.body.id);
+    event.set('Command__c', 'CLOSE_CASE');
+    org.insert({
+      sobject: event
+    }, err => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("IANA_case_creator__e published");
+      }
     });
+    res.redirect('/cases');
+  } else if (req.body.delete != null) {
+    var cs = nforce.createSObject('Case');
+    cs.set('Id', req.body.id);
+    console.log('id: %s', req.body.id);
+    org.delete({
+        sobject: cs
+      })
+      .then(function (msg) {
+        res.render('message', {
+          title: 'Case Deleted: ' + String(req.body.id)
+        });
+      });
+  }
 });
 
 router.post('/delete-account', function (req, res, next) {
@@ -142,9 +199,13 @@ router.post('/delete-account', function (req, res, next) {
   var acc = nforce.createSObject('Account');
   acc.set('Id', req.body.id);
   console.log('id: %s', req.body.id);
-  org.delete({ sobject: acc })
+  org.delete({
+      sobject: acc
+    })
     .then(function (msg) {
-      res.render('message', { title: 'Account Deleted: ' + String(req.body.id) });
+      res.render('message', {
+        title: 'Account Deleted: ' + String(req.body.id)
+      });
     });
 });
 
@@ -153,9 +214,13 @@ router.post('/delete-contact', function (req, res, next) {
   var acc = nforce.createSObject('Contact');
   acc.set('Id', req.body.id);
   console.log('id: %s', req.body.id);
-  org.delete({ sobject: acc })
+  org.delete({
+      sobject: acc
+    })
     .then(function (msg) {
-      res.render('message', { title: 'Contact Deleted: ' + String(req.body.id) });
+      res.render('message', {
+        title: 'Contact Deleted: ' + String(req.body.id)
+      });
     });
 });
 
@@ -165,12 +230,17 @@ router.get('/account/:id', function (req, res) {
 
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Force.com Sample' });
+  res.render('index', {
+    title: 'Force.com Sample'
+  });
 });
 
 router.get('/test/query', function (req, res) {
   var query = 'SELECT Id, Name, CreatedDate FROM Account ORDER BY CreatedDate DESC LIMIT 5';
-  org.query({ query: query, oauth: req.session.oauth }, function (err, resp) {
+  org.query({
+    query: query,
+    oauth: req.session.oauth
+  }, function (err, resp) {
     if (!err) {
       res.render('query', {
         title: 'query results',
@@ -181,13 +251,4 @@ router.get('/test/query', function (req, res) {
     }
   });
 });
-
-notifier.on('click', function (notifierObject, options) {
-  // Triggers if `wait: true` and user clicks notification
-});
-
-notifier.on('timeout', function (notifierObject, options) {
-  // Triggers if `wait: true` and notification closes
-});
-
 module.exports = router;
