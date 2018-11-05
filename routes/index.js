a4b8a452c9661a768f03b28be71748503349bfcb
Cecilia Ulrich <cecilia.ulrich@logicline.de>
2018-11-05 13:45:45 +0100

    Use config vars for credentials

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var notifier = require('node-notifier');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  if (org == null) {
+  if (org.oauth == null) {
     res.render('configuration', {
       title: 'Configuration'
     });
